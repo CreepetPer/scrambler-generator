cc79967b03eb51485e4ef62e3fbea320f0a3086f
CreepetPer <creepetper@gmail.com>
2026-08-12 17:39:51 +0800

    feat: complete scramble generation with additional face options and for loop logic

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-const faces = ['U', 'D', 'R', 'L', 'B', 'F'];
+const faces = ["U", "D", "R", "L", "B", "F", "U2", "D2", "R2", "L2", "B2", "F2", "U'", "D'", "R'", "L'", "B'", "F'"];
 let scramble = '';
 
 function generateScramble() {
-    scramble = faces[Math.floor(Math.random() * faces.length)];
-    return scramble;
+    scramble = '';
+    for (let i = 0; i <= 20; i++) {
+        currentFace = faces[Math.floor(Math.random() * faces.length)];
+        if (scramble === '') {
+            scramble += currentFace;
+        } else {
+            scramble += ` ${currentFace}`;
+        }
+
+    } return scramble;
 }
