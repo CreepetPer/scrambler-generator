6b859877920a74ac8f45456e36e54f20bf02661a
CreepetPer <creepetper@gmail.com>
2026-08-12 18:07:30 +0800

    feat: add DOM and btn event listener logic

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
+let scrambleBtn = document.getElementById('generate-scramble-btn');
+let scrambleDisplay = document.getElementById('scramble-display');
+
 const faces = ["U", "D", "R", "L", "B", "F", "U2", "D2", "R2", "L2", "B2", "F2", "U'", "D'", "R'", "L'", "B'", "F'"];
 let scramble = '';
 
 function generateScramble() {
     scramble = '';
-    for (let i = 0; i <= 20; i++) {
-        currentFace = faces[Math.floor(Math.random() * faces.length)];
+
+    for (let i = 0; i < 20; i++) {
+        let currentFace = faces[Math.floor(Math.random() * faces.length)];
         if (scramble === '') {
             scramble += currentFace;
         } else {
             scramble += ` ${currentFace}`;
         }
+    }
 
-    } return scramble;
+    scrambleDisplay.textContent = scramble;
+    return scramble;
 }
+
+scrambleBtn.addEventListener('click', () => {
+    generateScramble();
+});
